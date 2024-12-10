c2d016f5b7ac6e55c1110124ff25ed317da7c89f
Aman Gupta <51478388+amangupta05@users.noreply.github.com>
2024-12-10 18:42:20 -0500

    Add files via upload

---
+++ dashboard/App.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <h1>Emergency Response Dashboard</h1>
+            <p>Real-time updates will appear here.</p>
+        </div>
+    );
+}
+
+export default App;
